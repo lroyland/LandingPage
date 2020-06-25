4d73c40e6ba52d78234c33a30b52df2121f06344
Lucas Royland <roylucas@gmail.com>
2020-06-25 14:06:42 -0700

    Form views and Landing Page urls and settings edited.

--- Form/views.py
+++ Form/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
 
 # Create your views here.
+
+def form(request):
+    #return HttpResponse('<h1>Form without a form.</h1>')
+    return render(request, 'Form/index.html')
