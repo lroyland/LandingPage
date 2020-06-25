80e872715b93dde931f7ddd4713dfe02327dbdf3
Lucas Royland <roylucas@gmail.com>
2020-06-25 16:17:37 -0700

    Added Form models and initial migrations.

--- Form/models.py
+++ Form/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class newLead(models.Model):
+    first_name = models.CharField(max_length = 250)
+    last_name = models.CharField(max_length = 250)
+    email = models.CharField(max_length = 250)
+    phone_number = models.CharField(max_length = 100)
--- LandingPage/settings.py
+++ LandingPage/settings.py
@@ -57,8 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'Form/templates')
-
+            os.path.join(BASE_DIR, 'Form/templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
