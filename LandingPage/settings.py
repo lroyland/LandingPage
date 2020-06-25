fdc9e521abcf32e722db9798e2908eed6a4988a6
Lucas Royland <roylucas@gmail.com>
2020-06-25 14:35:17 -0700

    Added path for Form/templates to basedir in settings. And, added link to Bootstrap .css.

--- LandingPage/settings.py
+++ LandingPage/settings.py
@@ -56,7 +56,10 @@ ROOT_URLCONF = 'LandingPage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'Form/templates')
+
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
